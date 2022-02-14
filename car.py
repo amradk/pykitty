37cb6190177910d8c5083fe67fac0b56e5e9aa7e
amradk <greydog.dk@ya.ru>
2022-02-14 21:15:13 +0400

    fix problem with car X coordinate

--- car.py
+++ car.py
@@ -2,16 +2,21 @@ import pygame
 from gameobject import GameObject
 
 class Car(GameObject):
-    def __init__(self, color, speedx, start_x, start_y, animation_speed = 60 ):
-        GameObject.__init__(self, speedx, 0, start_x, start_y, animation_speed)
+    def __init__(self, color, speedx, start_x, start_y, width=32, heigh=32):
+        GameObject.__init__(self, speedx, 0, start_x, start_y, width, heigh)
         #self.image = pygame.image.load("assets/objects/shoots/laserGreen.png").convert_alpha()
         self.set_color(color)
+        self.is_active = True
         #self.rect = self.image.get_rect()
         #self.rect.bottom = start_y
         #self.rect.centerx = start_x
 
-    #def update(self):
-    #    self.rect.x += self.speedx
-    #    # kill if it moves off the top of the screen
-    #    if self.rect.top < 0:
-    #        self.kill()
+    def update(self):
+        if self.is_active == True:
+            super().update()
+
+    def set_inactive(self):
+        self.is_active = False
+
+    def set_active(self):
+        self.is_active = True
--- carmanager.py
+++ carmanager.py
@@ -5,24 +5,47 @@ from car import Car
 
 class CarManager():
     def __init__(self, screen, start_x, start_y, speed, direction, max_cars):
+        self.car_sizes = [32, 64, 96]
+        self.gap_sizes = [64, 96, 126]
         self.start_x = start_x
         self.start_y = start_y
         self.max_cars = max_cars
         self.direction = direction
         self.speed = self.direction * speed
-        self.gap = 96
         self.grp_car = pygame.sprite.Group()
         self.screen = screen
+        self.active_cars = max_cars
         (self.screen_width, self.screen_height) = self.screen.get_size()
 
+    def choose_speed(self):
+        self.speed = random.randrange(1,6) * self.direction
+
+
     def update(self):
-        for c in self.grp_car:
-            if self.direction > 0:
-                if c.get_x() > self.screen_width:
-                    c.set_x(self.start_x)
-            if self.direction < 0 :
-                if c.get_x() < 0:
-                    c.set_x(self.start_x)
+        if self.active_cars <= 0:
+            x = self.start_x
+            self.choose_speed()
+            for c in self.grp_car:
+                gap = random.choice(self.gap_sizes)
+                c.set_active()
+                c.set_speed_x(self.speed)
+                self.active_cars = self.active_cars + 1
+                c.set_x(x)
+                x = x + c.image.get_width() + gap
+
+        else:
+            for c in self.grp_car:
+                if self.direction > 0:
+                    if c.get_x() > 1024:
+                        c.set_inactive()
+                        self.active_cars = self.active_cars - 1
+                if self.direction < 0:
+                    if c.is_active == True:
+                        if c.get_x() < (0 - c.image.get_width()):
+                            #c.set_x(self.start_x)
+                            c.set_inactive()
+                            self.active_cars -= 1
+
         self.grp_car.update()
 
     def draw(self):
@@ -35,7 +58,7 @@ class CarManager():
         x = self.start_x
         for i in range(self.max_cars):
             color = list(random.choices(range(256), k=3))
-            c = Car(color, self.speed, x, self.start_y)
-            self.grp_car.add(c)
-            x = x + self.gap
-
+            car_size = random.choice(self.car_sizes)
+            gap = random.choice(self.gap_sizes)
+            self.grp_car.add(Car(color, self.speed, x, self.start_y, car_size, 32))
+            x = x + car_size + gap
