f299e7e1c4ee6c2e4b3ca81e6959cecaeb3d820d
amradk <greydog.dk@ya.ru>
2022-02-19 21:24:09 +0400

    move some const to separate file

---
+++ const.py
@@ -0,0 +1,6 @@
+WIN_WIDTH = 800 #Ширина создаваемого окна
+WIN_HEIGHT = 600 # Высота
+DISPLAY = (WIN_WIDTH, WIN_HEIGHT) # Группируем ширину и высоту в одну переменную
+BACKGROUND_COLOR = "#004400"
+TILE_WIDTH = 32
+TILE_HEIGHT = 32
